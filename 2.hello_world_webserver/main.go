0d4be19a4f021029e1fbfdb170c6df4d1eac8327
Chun Wu <chunw@nvidia.com>
2023-08-31 18:13:08 -0700

    README

---
+++ 2.hello_world_webserver/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+	fmt.Println("Useful resources for golang...")
+	fmt.Println("Go standard library documentation: https://pkg.go.dev/std")
+	fmt.Println("GoDoc - great for open source packages: https://pkg.go.dev/")
+	fmt.Println("The Mozilla Developer Network's Javascript Documentation: https://developer.mozilla.org/en-US/docs/Web/javascript")
+	fmt.Println("jsDelivr - a helpful CDN which hosts a lot of O/S projects: https://www.jsdelivr.com/")
+	fmt.Println("cdnjs - another great CDN with lots of externally hosted javascript and css resources: https://cdnjs.com/ ")
+	fmt.Println("The Mozilla Developer Network's documentation for CSS: https://developer.mozilla.org/en-US/docs/Web/CSS")
+}

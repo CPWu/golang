1f33c54adcdc83c82ac7d6ee45ff9c0693d92c6e
Chun Wu <chunw@nvidia.com>
2023-08-31 18:11:14 -0700

    Reorganizing golang learning repository.

---
+++ 3.variables_&_functions/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	var whatToSay string
+	var i int
+	whatToSay = "Goodbye, cruel world."
+
+	fmt.Println(whatToSay)
+
+	i = 7
+
+	fmt.Println("i is set to:", i)
+
+	whatWasSaid := saySomething()
+	fmt.Println("The function returned", whatWasSaid)
+}
+
+func saySomething() string {
+	return "something"
+}

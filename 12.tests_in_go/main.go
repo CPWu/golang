176d2c574cc3180eda5f3df40a82b3b04cbcfa8d
Chun Wu <chunw@nvidia.com>
2024-12-17 23:18:23 -0800

    golang: tests in go

--- 12.tests_in_go/main.go
+++ 12.tests_in_go/main.go
@@ -5,6 +5,10 @@ import (
 	"log"
 )
 
+// Concepts
+// Testing with Golang
+// In Golang, tests live right besides the code that is being tested with the naming convention *_test.go
+
 func main() {
 	result, err := divide(100.0, 10.0)
 	if err != nil {
@@ -12,15 +16,17 @@ func main() {
 		return
 	}
 
-	log.Println("The result of division is:, ", result)
+	log.Println("The result of division is:", result)
 }
 
 func divide(x, y float32) (float32, error) {
 	var result float32
 
+	// Division by zero is no good, so we test for this
 	if y == 0 {
-		return result, errors.New("Cannot divide by zero.")
+		return result, errors.New("cannot divide by zero")
 	}
+
 	result = x / y
 	return result, nil
 }
--- 12.tests_in_go/main_test.go
+++ 12.tests_in_go/main_test.go
@@ -2,6 +2,11 @@ package main
 
 import "testing"
 
+// Run `go test -cover for Test Coverage`
+// OR
+// `go test -coverprofile=coverage.out && go tool cover -html=coverage.out`
+// Table Tests
+// tests is equivalent to slice of structs, each struct defines a test case to run.
 var tests = []struct {
 	name     string
 	dividend float32
@@ -14,7 +19,10 @@ var tests = []struct {
 	{"expect-five", 50.0, 10.0, 5.0, false},
 }
 
+// To write a test in go, you need to give it a name and it has to start with the word test.
+// Run tests by running `go test` or `go test -v`
 func TestDivision(t *testing.T) {
+	// Range over test variable
 	for _, tt := range tests {
 		got, err := divide(tt.dividend, tt.divisor)
 		if tt.isErr {

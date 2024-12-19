189d19ddec5126384a26345e0d64531d1aedad33
Chun Wu <chunw@nvidia.com>
2024-12-18 22:19:39 -0800

    golang: template cache

---
+++ 21.template_cache/cmd/web/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/cpwu/golang/pkg/handlers"
+)
+
+// Concepts
+// Template Cache
+
+// Global port number for web server
+const portNumber string = ":8080"
+
+// main is the main application function
+func main() {
+
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Printf("Starting the application on port: %s", portNumber)
+
+	_ = http.ListenAndServe(portNumber, nil)
+
+}
